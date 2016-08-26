1ab56bb8be4163f8233af5030d2b581dcfb8abf3
Richa Verma <bansalr@us.ibm.com>
2016-08-26 14:35:56 -0500

    Webpack Template

--- app/index.js
+++ app/index.js
@@ -1,6 +1,1 @@
-const angular = require('angular');
-
-var HelloAngular = angular.module('HelloAngular',[])
-  .controller('HelloAngularController', function($scope){
-    $scope.greeting = "Hello Angular";
-  });
+console.log('Hello Webpack!');
