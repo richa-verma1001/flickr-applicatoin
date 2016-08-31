5ec266720f34f4527f8ab509f95c5eedfc203551
Richa Verma <bansalr@us.ibm.com>
2016-08-31 16:10:24 -0500

    Basic Routing template added using angular-ui-router * Routes to 2 states, Hello and About ['hello' defaultRoute] * Navigation links added for routes * route views updates only layout section i.e rightPane * Next Steps, try nested routing

--- app/app.js
+++ app/app.js
@@ -1,13 +1,29 @@
 import angular from 'angular';
-import angularRoute from 'angular-route';
+//import angularRoute from 'angular-route';
+import 'angular-ui-router';
 import greetUser from './components/greetUser/greetUser.component';
 import aboutUser from './components/aboutUser/aboutUser.component';
 
 const moduleName = 'myApp';
-var myApp = angular.module(moduleName, [angularRoute, greetUser, aboutUser]);
-console.log('MyApp' + myApp);
+var myApp = angular.module(moduleName, ['ui.router', greetUser, aboutUser]);
 
+myApp.config(['$stateProvider', '$urlRouterProvider',
+  function config($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/state1");
 
+    $stateProvider
+      .state('hello',{
+        url: "/hello",
+        template: '<greet-user></greet-user>'
+      })
+      .state('about',{
+        url: "/about",
+        template: '<about-user></about-user>'
+      })
+  }
+]);
+
+/*
 myApp.config(['$locationProvider', '$routeProvider',
     function config($locationProvider, $routeProvider) {
       $locationProvider.hashPrefix('!');
@@ -21,6 +37,6 @@ myApp.config(['$locationProvider', '$routeProvider',
       }).
       otherwise('/hello');
     }
-]);
+]); */
 
 export default moduleName;
