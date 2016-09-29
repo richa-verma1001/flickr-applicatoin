390e74142e7c299f138f9a7fc7ee0e8b08ce351d
Richa Verma <bansalr@us.ibm.com>
2016-09-29 10:43:56 -0500

    Cleanup the Flick Application

--- app/services/photos.service.js
+++ app/services/photos.service.js
@@ -1,24 +1,32 @@
 import angular from 'angular';
 
 class PhotosService {
+  //const self = this;
+  //var photos = {};
+
   constructor($http){
     this.$http = $http;
-    //this.processPhotos();
+    this.photos = {};
+    //this.getImgUrl();
   }
 
   getPhotos() {
     let promise;
-
     promise = this.$http({
       method: 'GET',
       url: 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=24662369@N07&format=json&nojsoncallback=1',
-      cache: true,  // true only if you want the response to be cached
+      cache: true,
     });
     return promise;
   }
 
   getImgUrl(photo) {
+    let promise = this.getPhotos();
 
+    promise.then(function (response){
+      this.photos = response.data.photos.photo;
+      console.log(this.photos);
+    });
   }
 }
 
